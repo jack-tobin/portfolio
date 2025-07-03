6bef31d00471df0794b8356d783d82b123a2a7c1
Jack Tobin, CFA <52289562+jack-tobin@users.noreply.github.com>
2025-07-03 22:36:58 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const viewport: Viewport = {
 }
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://jack-tobin.vercel.app/'),
+  metadataBase: new URL('https://jacktobin.dev/'),
   alternates: {
     canonical: '/',
   },
